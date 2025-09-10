4195ea84e1e9bc2e982313ef6a7f63f187acdc31
codedbyminjae <minjaekim7111@naver.com>
2025-09-10 11:30:30 +0900

    Add: dtype 바꾸기 + 차원 바꾸기

--- week01/change_array_roperties.py
+++ week01/change_array_roperties.py
@@ -9,3 +9,19 @@ print(b)
 
 c = np.uint8(a) # dtype 함수를 사용할 수도 있다.
 print(c)
+
+# 차원 바꾸기
+a = np.arange(6)
+print(a)
+
+b = a.reshape(2, 3)
+print(b)
+
+c = np.reshape(a, (2, 3))
+print(c)
+
+d = np.arange(100).reshape(2, -1)
+print(d)
+
+e = np.arange(100).reshape(-1, 5)
+print(e)
