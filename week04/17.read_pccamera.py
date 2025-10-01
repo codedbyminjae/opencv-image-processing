a3734d8631a553926b0d392bd006853be09b9c35
codedbyminjae <minjaekim7111@naver.com>
2025-10-01 09:49:04 +0900

    read pccamer

--- week04/17.read_pccamera.py
+++ week04/17.read_pccamera.py
@@ -11,6 +11,8 @@ capture = cv2.VideoCapture(0)  # 0번 카메라 연결
 if capture.isOpened() == False:
     raise Exception("카메라 연결 안됨")
 
+# 보통 1초에 30프레임
+
 # 카메라 속성 획득 및 출력
 print("너비 %d" % capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 print("높이 %d" % capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -19,11 +21,14 @@ print("밝기 %d" % capture.get(cv2.CAP_PROP_BRIGHTNESS))
 
 while True:  # 무한 반복
     ret, frame = capture.read()  # 카메라 영상 받기
-    if not ret: break
-    if cv2.waitKey(30) >= 0: break
+    # 해당 부분에서 처리를 넣어줘서 화면을 보여주는 기능을 추가할 수 있다.
+    if not ret:
+        break
+    if cv2.waitKey(30) >= 0:
+        break
 
     exposure = capture.get(cv2.CAP_PROP_EXPOSURE)
-    put_string(frame, "EXPOS: ", (10, 40), exposure)
+    put_string(frame, "EXPOS: ", (10, 40), exposure) # 해당 부분에서 상단 부분에서 화면상의 text를 보여주는 중
     title = "View Frame from Camera"
     cv2.imshow(title, frame)  # 윈도우에 영상 띄우기
 capture.release()
