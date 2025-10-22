e07f7be528ddd4788c6bc4d1b04b122c6ea89c8f
codedbyminjae <minjaekim7111@naver.com>
2025-10-22 10:55:04 +0900

    명암 대비

--- week07/06.contrast.py
+++ week07/06.contrast.py
@@ -6,7 +6,9 @@ if image is None: raise Exception("영상 파일 읽기 오류 발생")
 noimage = np.zeros(image.shape[:2], image.dtype)        # 더미 영상
 avg = cv2.mean(image)[0]/2.0                            # 영상 화소 평균의 절반
 
-dst1 = cv2.scaleAdd(image, 0.2, noimage) + 20               # 영상대비 감소
+dst1 = cv2.scaleAdd(image, 0.5, noimage) + 20               # 영상대비 감소
+# dst1 = image * 0.5
+# dst1 = dst1.astype('unit8')
 dst2 = cv2.scaleAdd(image, 2.0, noimage)                # 영상대비 증가
 dst3 = cv2.addWeighted(image, 0.5, noimage, 0, avg)     # 명암대비 감소
 dst4 = cv2.addWeighted(image, 2.0, noimage, 0,-avg)     # 명암대비 증가
@@ -20,5 +22,3 @@ cv2.imshow("dst4 - increase contrast using average", dst4)
 
 cv2.imwrite("dst.jpg",dst1)
 cv2.waitKey(0)
-
-
