64e8d0593b9226f80776200157bdbac83cdbfd80
codedbyminjae <minjaekim7111@naver.com>
2025-11-12 10:15:44 +0900

    블러링 & 샤프닝

---
+++ week09/gausian.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('images/filter_blur.jpg')
+
+k1 = np.array([[1,2,1],
+               [2,4,2],
+               [1,2,1]]) * (1/16)
+
+blur1 = cv2.filter2D(img, -1, k1)
+k2 = cv2.getGaussianKernel(7, 0)
+
+blur2 = cv2.filter2D(img, -1, k2*k2.T)
+
+blur3 = cv2.GaussianBlur(img, (3, 3), 0)
+
+print('k1:', k1)
+print('k2:', k2*k2.T)
+
+merged = np.hstack((img, blur1, blur2, blur3))
+cv2.imshow("dst3", merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
